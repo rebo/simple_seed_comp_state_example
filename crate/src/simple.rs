8887d3b303f3a955efdc58b49836964ba96bf820
rebo <rebo@fakeemail.org>
2019-11-27 20:30:10 +0000

    using use_istate instead of use_state for cleaner api.

--- crate/src/simple.rs
+++ crate/src/simple.rs
@@ -1,7 +1,7 @@
 #![allow(non_snake_case)]
 use crate::generated::css_classes::C;
 
-use comp_state::{topo, use_state};
+use comp_state::{topo, use_istate};
 use seed::dom_types::UpdateEl;
 use seed::prelude::*;
 use seed::*;
@@ -24,12 +24,10 @@ pub fn view() -> Node<Msg> {
 }
 
 fn render_button() -> Node<Msg> {
-    topo::call!({
-        let (count, count_access) = use_state(|| 0);
-        button![
-            class![C.p_4, C.bg_gray_4, C.rounded, C.shadow, C.m_4],
-            format!("Pressed {} times", count),
-            on_click(move |_| count_access.set(count + 1))
-        ]
-    })
+    let (count, count_access) = use_istate(|| 0);
+    button![
+        class![C.p_4, C.bg_gray_4, C.rounded, C.shadow, C.m_4],
+        format!("Pressed {} times", count),
+        on_click(move |_| count_access.set(count + 1))
+    ]
 }
